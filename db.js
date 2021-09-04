9f70bf35c4ab5cf313d235bc82c97609563d461c
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-09-04 12:45:11 +0000

    clinup db file

--- db.js
+++ db.js
@@ -1,19 +1,9 @@
 const users = {
-  /*"b2xVn2": {
-    userID: "b2xVn2", 
-    email: "user@example.com", 
-    password: "example"
-  },
- "9sm5xK": {
-    userID: "9sm5xK", 
-    email: "user2@example.com", 
-    password: "example"
-  }*/
+
 };
 
 const urlDatabase = {
-  // "k1MjSN": {longURL:"http://www.lighthouselabs.ca", userID: "b2xVn2"},
-  // "CvJ0Is": {longURL:"http://www.google.com", userID: "9sm5xK"}
+  
 };
 
 module.exports = { users, urlDatabase };
